2a1a5c189833288b410f63a9020d781ecc257f70
maja3494 <45010478+maja3494@users.noreply.github.com>
2018-11-29 14:15:47 -0700

    Update RiverSource.hpp

--- RiverSource.hpp
+++ RiverSource.hpp
@@ -7,6 +7,12 @@ struct River
 {
 	string riverName;
 	section* sections;
+	River *nextRiver;
+	River(string name)
+	{
+		this->riverName=name;
+		this->sections=this->nextRiver=nullptr;
+	}
 };
 struct Section
 {
@@ -14,7 +20,7 @@ struct Section
 	string secName;
 	string rapidClass;
 	double sectionLength;
-	section* nextSection;
+	Section* nextSection;
 	rapid* firstRapid;
 	note* notes;
 };
@@ -29,59 +35,66 @@ struct Rapid
 struct waterLevel
 {
 	string date;
-	int waterLevel;	
+	int waterLevel;
 };
 struct Note
 {
 	string aNote;
 	note* nextNote;
 };
-public:
-	//constructor
-	RiverSource();
 
-	//deconstructor
-	~riverSource();
+class RiverSource
+{
+	public:
+		//constructor
+		RiverSource();
 
-	//find the best day to run a certain section given the name of the section
-	void bestDayOnSection(string name);
+		//deconstructor
+		~riverSource();
 
-	//display the beta for a run
-	//outputs the mileage for each rapid in a section 
-	//outputs the beta for each rapid
-	void diplayBeta(section *wantedSection);
 
-	//given a start and stop section, output all raipids and their beta
-	//with the correct mileages
-	void linkSections(section *start,section *stop);
+		void createSection();
 
-	//displays a water water level for a given day
-	void diplayWaterLevel(section *wantedSection, string date);
 
-	//find the best section to run on a given day
-	//takes in the class, date and wanted mileage
-	void findBestSection(string wantedClass, string date, int mileage);
+		//find the best day to run a certain section given the name of the section
+		void bestDayOnSection(string name);
 
-	//adds the river names from a csv
-	//prompts user for the file name and reads in the river names
-	void addRiverNames();
+		//display the beta for a run
+		//outputs the mileage for each rapid in a section
+		//outputs the beta for each rapid
+		void diplayBeta(section *wantedSection);
 
-	//add beta for a section from a file
-	void addBeta();
+		//given a start and stop section, output all raipids and their beta
+		//with the correct mileages
+		void linkSections(section *start,section *stop);
 
-	//add section names from a file
-	//promts user for a river and for file name
-	void addSection();
+		//displays a water water level for a given day
+		void diplayWaterLevel(section *wantedSection, string date);
 
-	//displays an entire river from head to end
-	//takes in the name of the river
-	void diplayEntireRiver(string riverName);
+		//find the best section to run on a given day
+		//takes in the class, date and wanted mileage
+		void findBestSection(string wantedClass, string date, int mileage);
 
-	//find the location of a section
-	section* findSection(string river, string secitonName);
-private:
-int numberOfRivers=15;
-River rivers[numberOfRivers];
-int currentLength;
+		//adds the river names from a csv
+		//prompts user for the file name and reads in the river names
+		void addRiverNames();
 
+		//add beta for a section from a file
+		void addBeta();
+
+		//add section names from a file
+		//promts user for a river and for file name
+		void addSection();
+
+		//displays an entire river from head to end
+		//takes in the name of the river
+		void diplayEntireRiver(string riverName);
+
+		//find the location of a section
+		section* findSection(string river, string secitonName);
+	private:
+		int numberOfRivers=15;
+		River rivers[numberOfRivers];
+		int currentLength;
+};
 #endif
