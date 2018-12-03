4e44d19e71ba52c9b854480bca644b9b00d4ca09
maja3494 <45010478+maja3494@users.noreply.github.com>
2018-12-03 16:21:05 -0700

    Update RiverSource.cpp

--- RiverSource.cpp
+++ RiverSource.cpp
@@ -5,6 +5,21 @@
 #include <sstream>
 using namespace std;
 
+RiverSource::RiverSource(int riverSize,int sectionSize)
+{
+  numberOfRivers=riverSize;
+  numberOfSections=sectionSize;
+  Section *sectionArray= new Section[numberOfSections];
+  River *riverArray=new River[numberOfRivers];
+  rivers=riverArray;
+  sections=sectionArray;
+}
+
+RiverSource::~RiverSource()
+{
+
+}
+
 int RiverSource::hashRiver(string name)
 {
 	int key=0;
@@ -25,23 +40,23 @@ int RiverSource::hashSection(string name)
 	key=key%numberOfSections;
 	return key;
 }
-void RiverSource::buildSectionHash()
-{
-	int key;
-	Section newSection;
-	for (int i=0;i<15;i++)
-	{
-		
-			Section *current=rivers[i].firstSection;
-			while (current!=nullptr)
-			{
-				key=hashSection(current->secName);
-				/////////////
-				current=current->nextSection;
-			}
-	    
-	}
-}
+// void RiverSource::buildSectionHash()
+// {
+// 	int key;
+// 	Section newSection;
+// 	for (int i=0;i<15;i++)
+// 	{
+//
+// 			Section *current=rivers[i].firstSection;
+// 			while (current!=nullptr)
+// 			{
+// 				key=hashSection(current->secName);
+// 				/////////////
+// 				current=current->nextSection;
+// 			}
+//
+// 	}
+// }
 void RiverSource::mergeRivers(River* feeder, Section *mainStream)
 {
 	Section *temp=feeder->firstSection;
@@ -54,54 +69,57 @@ void RiverSource::mergeRivers(River* feeder, Section *mainStream)
 River* RiverSource::searchRiver(string name)
 {
 	int key= hashRiver(name);
-	River current=rivers[key];
+	River *current=rivers[key];
 	while(current!=nullptr)
 	{
-		if (current.riverName==name)
+		if (current->riverName==name)
 		{
-			return *current;
+			return &current;
 		}
 		current=current->nextRiver;
 	}
 	return nullptr;
 }
+
 Section* RiverSource::searchSections(string name)
 {
-	Section* location;
 	int key=hashSection(name);
-	Section *current=&sections[key];
-	while(current!=nullptr)
-	{
-		if (current->secName==name)
-			{
-				return current;
-			}
-		current=current->nextSection;
-	}
+	Section *current=sections[key];
+  if(current!=NULL)
+  {
+    while(current->secName!=name && current!=NULL)
+  	{
+  		if (current->secName==name)
+  			{
+  				return current;
+  			}
+  		current=current->nextSection;
+  	}
+  }
 	cout<<"section not found"<<endl;
 	return nullptr;
 }
 void RiverSource::addRiver(River *river)
 {
 	int key=hashRiver(river->riverName);
-	River location=*rivers[key];
+	River *location=rivers[key];
 	if(location==nullptr)
 		{
 			location=river;
 		}
 	else
 	{
-		while(location.nextRiver!=nullptr)
+		while(location->nextRiver!=nullptr)
 		{
-			location=location.nextRiver;
+			location=location->nextRiver;
 		}
-		location.nextRiver=river;
+		location->nextRiver=river;
 	}
 }
 void RiverSource::addSection(River *name, Section* sectionToAdd)
 {
 	int key=hashRiver(name->riverName);
-	Section *temp=rivers[key].firstSection;
+	Section temp=rivers[key].firstSection;
 	while (temp->nextSection!=nullptr)
 		{
 			temp=temp->nextSection;
@@ -111,7 +129,7 @@ void RiverSource::addSection(River *name, Section* sectionToAdd)
 
 int main()
 {
-  RiverSource Source;
+  RiverSource Source(15,20);
   ifstream riverFile("riverRead.txt");
   string line;
   string riverName;
