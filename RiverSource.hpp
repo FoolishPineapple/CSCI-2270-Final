19553f63386cda3863b9df71ba9dadd48283c901
maja3494 <45010478+maja3494@users.noreply.github.com>
2018-12-10 15:48:51 -0700

    Update RiverSource.hpp

--- RiverSource.hpp
+++ RiverSource.hpp
@@ -93,47 +93,13 @@ class RiverSource
 		//deconstructor
 		~RiverSource();
 
-
-		void createSection();
-
-
-		//find the best day to run a certain section given the name of the section
-		void bestDayOnSection(string name);
-
-		//given a start and stop section, output all raipids and their beta
-		//with the correct mileages
-		void linkSections(Section *start,Section *stop);
-
-		//displays a water water level for a given day
-		void diplayWaterLevel(Section *wantedSection, string date);
-
-		//find the best section to run on a given day
-		//takes in the class, date and wanted mileage
-		void findBestSection(string wantedClass, string date, int mileage);
-
-		//adds the river names from a csv
-		//prompts user for the file name and reads in the river names
-		void addRiverNames();
-
-		//add beta for a section from a file
-		void addBeta();
-
-		//add section names from a file
-		//promts user for a river and for file name
 		void addSection(River *riverName, Section* sectionToAdd);
 
-		//displays an entire river from head to end
-		//takes in the name of the river
 		void displayEntireRiver(string riverName,int data);
-
-		//find the location of a section
-		Section* findSection(string river, string secitonName);
 
 		int hashRiver(string name);
 
 		int hashSection(string name);
-
-		void buildSectionHash();
 
 		River* searchRiver(string name);
 
@@ -160,8 +126,6 @@ class RiverSource
 		WaterLevel getBestDay(Section *currentSection);
 
 		int getDayOfYear(int day, int month);
-
-		Section* getBestSecction(int date);
 
 		void findMySection(double mileage, string rapClass);
 
